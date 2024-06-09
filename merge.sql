b1ceafec80f315057800c75a05a2eb8da5450634
Eric Cameron <ericcameronnj@gmail.com>
2024-06-08 21:52:28 -0400

    SQL Merge Command

--- merge.sql
+++ merge.sql
@@ -1,8 +1,8 @@
-DROP TABLE students;
+DROP TABLE student;
 DROP TABLE imported;
 
--- Create a simple students table
-CREATE TABLE students
+-- Create a simple student table
+CREATE TABLE student
 (
 	studentID INT PRIMARY KEY,
 	firstname CHAR(20),
@@ -25,29 +25,29 @@ CREATE TABLE imported
     zip CHAR(5)
 );
 
--- populate students table with students 
-INSERT INTO students (studentID, firstname, lastname, address, city, state, zip) VALUES
-	(1,'Lara', 'Garcia', '850 Shady Beach Ave', 'Stockholm', 'NJ', '07460'),
-	(2,'Pola', 'Hayek', '430 Brial Hwy', 'Paterson', 'NJ', '07513'),
-	(3,'Martin', 'Bryan', '1199 Baum Ln', 'Hawthorne', 'NJ', '07506'),
-	(4,'Hiroshi', 'Tanaka', '2152 Kylie Pl','Pompton Lakes', 'NJ', '07442'),
-	(5,'Yoshio', 'Ito', '211 Cameron Rd', 'Ringwood','NJ','07456')
+-- populate student table with students 
+INSERT INTO student (studentID, firstname, lastname, address, city, state, zip) VALUES
+	(1, 'Lara', 'Garcia', '850 Shady Beach Ave', 'Stockholm', 'NJ', '07460'),
+	(2, 'Pola', 'Hayek', '430 Brial Hwy', 'Paterson', 'NJ', '07513'),
+	(3, 'Martin', 'Bryan', '1199 Baum Ln', 'Hawthorne', 'NJ', '07506'),
+	(4, 'Hiroshi', 'Tanaka', '2152 Kylie Pl','Pompton Lakes', 'NJ', '07442'),
+	(5, 'Yoshio', 'Ito', '211 Cameron Rd', 'Ringwood','NJ','07456')
 	;
 
 -- two new students (#6 and #7)
 -- Lara Garcia (#1) changed her last name
 -- Pola Hayek (#2) changed address
 INSERT INTO imported (studentID, firstname, lastname, address, city, state, zip) VALUES
-	(1,'Lara', 'Mueller', '850 Shady Beach Ave', 'Stockholm', 'NJ', '07460'),
-	(2,'Pola', 'Hayek', '745 Merle Blvd', 'Clifton', 'NJ', '07014'),
-	(6,'Juvina', 'Pawar', '153 Surrey Rd', 'Hawthorne', 'NJ', '07507'),
-	(7,'Li', 'Gao', '1303 Lakina Ln','Totowa', 'NJ', '07511')
+	(1, 'Lara', 'Mueller', '850 Shady Beach Ave', 'Stockholm', 'NJ', '07460'),
+	(2, 'Pola', 'Hayek', '745 Merle Blvd', 'Clifton', 'NJ', '07014'),
+	(6, 'Juvina', 'Pawar', '153 Surrey Rd', 'Hawthorne', 'NJ', '07507'),
+	(7, 'Li', 'Gao', '1303 Lakina Ln', 'Totowa', 'NJ', '07511')
 	;
 
 -- Start the merge; specify primary key
-MERGE students 
+MERGE student 
     USING imported 
-    ON imported.studentid = students.studentid
+    ON imported.studentid = student.studentid
 
     -- For updated records (found in both tables), perform an UPDATE query
     WHEN MATCHED THEN UPDATE SET
@@ -59,8 +59,8 @@ MERGE students
     state = imported.state,
     zip = imported.zip
 
-   -- For records found in the imported table, but not students, do an INSERT
-   -- "TARGET" refers to the main table; in our case, students
+   -- For records found in the imported table, but not student, do an INSERT
+   -- "TARGET" refers to the main table; in our case, student
     WHEN NOT MATCHED BY TARGET THEN
     INSERT (studentID, firstname, lastname, address, city, state, zip) VALUES 
     (imported.studentID, imported.firstname, imported.lastname, imported.address, imported.city, imported.state, imported.zip)
@@ -75,7 +75,7 @@ MERGE students
 
 -- Can view merged table here:
 SELECT *
-FROM students;
+FROM student;
 
 -- can probably safely drop the imported table now
 DROP table imported;
